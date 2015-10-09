999f2a6f71cd47073408056d944097268472e4e2
Alan Ward <arw180@gmail.com>
2015-10-09 14:35:31 -0400

    return id with agency request

--- ozpcenter/api/agency/serializers.py
+++ ozpcenter/api/agency/serializers.py
@@ -16,7 +16,7 @@ class AgencySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Agency
         depth = 2
-        fields = ('title', 'short_name')
+        fields = ('title', 'short_name', 'id')
 
 class MinimalAgencySerializer(serializers.ModelSerializer):
     class Meta:
