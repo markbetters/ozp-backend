2f61b1da79f52ef7cf4c8b82618d229539259cbb
Manny Rivera <mannyrivera2010@gmail.com>
2016-12-08 10:05:43 -0500

    feat(elasticsearch): fixed private apps

--- ozpcenter/api/listing/tests/test_elasticsearch_util.py
+++ ozpcenter/api/listing/tests/test_elasticsearch_util.py
@@ -4,6 +4,7 @@ Elasticsearch Utils tests
 Test this class indiviual with command:
 python manage.py test ozpcenter.api.listing.tests.test_elasticsearch_util
 """
+from unittest import skip
 from django.test import TestCase
 
 from ozpcenter.scripts import sample_data_generator as data_gen
@@ -24,9 +25,158 @@ class ElasticsearchUtilTest(TestCase):
         Set up test data for the whole TestCase (only run once for the TestCase)
         """
         data_gen.run()
+        # elasticsearch_util.
 
     def test_get_mapping_setting_obj(self):
         mapping_obj = elasticsearch_util.get_mapping_setting_obj()
         self.assertTrue('settings' in mapping_obj)
+
+    @skip("TODO Finish (rivera 20161207)")
+    def test_prepare_clean_listing_record(self):
+        raw_record = {}
+        clean_record = elasticsearch_util.prepare_clean_listing_record(raw_record)
+        self.assertTrue('settings' in clean_record)
+
+    @skip("TODO Finish (rivera 20161207)")
+    def test_make_search_query_obj(self):
+        expected = {
+          "min_score": 0.4,
+          "query": {
+            "bool": {
+              "should": [{
+                  "match": {
+                    "title": {
+                      "query": "Clipboard",
+                      "boost": 10
+                    }}},
+                {"match": {
+                    "description": {
+                      "query": "Clipboard",
+                      "boost": 3
+                    }}},
+                {"match": {
+                    "description_short": {
+                      "query": "Clipboard",
+                      "boost": 3
+                    }}},
+                {"nested": {
+                    "boost": 1,
+                    "query": {
+                      "query_string": {
+                        "query": "Clipboard",
+                        "fields": [
+                          "tags.name"
+                        ]
+                      }},
+                    "path": "tags"
+                  }}],
+              "filter": [
+                {"query": {
+                    "term": {
+                      "is_deleted": 0
+                    }}},
+                {"query": {
+                    "term": {
+                      "is_enabled": 1
+                    }}},
+                {"query": {"match": {
+                    "approval_status": "APPROVED"
+                    }}},
+                {"query": {"bool": {
+                    "should": [
+                        {"match": {
+                            "agency_short_name": "Minitrue"
+                          }},
+                        {"match": {
+                            "agency_short_name": "Miniluv"
+                          }}
+                      ]}}},
+                {"query": {
+                    "bool": {
+                      "must_not": [
+                        {"bool": {
+                            "filter": [
+                              {
+                                "match": {
+                                  "agency_short_name": "Minipax"
+                                }
+                              },
+                              {
+                                "match": {
+                                  "is_private": 1
+                                }
+                              }
+                            ]
+                          }
+                        },
+                        {"bool": {
+                            "filter": [{
+                                "match": {
+                                  "agency_short_name": "Minitrue"
+                                }
+                              },
+                              {
+                                "match": {
+                                  "is_private": 1
+                                }
+                              }
+                            ]}},
+                        {
+                          "bool": {
+                            "filter": [
+                              {
+                                "match": {
+                                  "agency_short_name": "Ministry of Plenty"
+                                }
+                              },
+                              {
+                                "match": {
+                                  "is_private": 1
+                                }
+                              }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                  }
+                },
+                {
+                  "query": {
+                    "bool": {
+                      "should": [
+                        {
+                          "match": {
+                            "listing_type_title": "web application"
+                          }
+                        }
+                      ]
+                    }
+                  }
+                },
+                {
+                  "nested": {
+                    "boost": 1,
+                    "query": {
+                      "bool": {
+                        "should": [
+                          {
+                            "match": {
+                              "categories.title": "Education"
+                            }
+                          }
+                        ]
+                      }
+                    },
+                    "path": "categories"
+                  }
+                }
+              ]
+            }
+          },
+          "size": 24
+        }
+
+        self.assertTrue('size' in expected)
 
     # TODO Add more test
