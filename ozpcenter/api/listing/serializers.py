80d5882c7e5f3d9c573e280aff26739b300d60d2
Michael Lee <michael.lee@bia-boeing.com>
2017-04-17 16:03:35 -0400

    fix(screenshot) Fixed bug for PUT when description is null

--- ozpcenter/api/listing/serializers.py
+++ ozpcenter/api/listing/serializers.py
@@ -950,7 +950,7 @@ class ListingSerializer(serializers.ModelSerializer):
                 obj, created = models.Screenshot.objects.get_or_create(
                     small_image=new_small_image,
                     large_image=new_large_image,
-                    description=s['description'],
+                    description=s.get('description'),
                     listing=instance)
 
                 new_screenshot_instances.append(obj)
