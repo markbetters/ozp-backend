c4c965bc0901b0b4ba5023627a795e452ebdab50
Manny Rivera <mannyrivera2010@gmail.com>
2017-04-21 15:13:27 -0400

    refactor(sql): Added SQL for User Permissions

--- ozpcenter/api/storefront/model_access.py
+++ ozpcenter/api/storefront/model_access.py
@@ -5,8 +5,8 @@ import logging
 
 from django.db.models.functions import Lower
 from django.db import connection
-from django.forms.models import model_to_dict
 import msgpack
+
 
 import ozpcenter.api.listing.serializers as listing_serializers
 from ozpcenter import models
@@ -25,19 +25,48 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
+# SELECT
+#   ozpcenter_profile.id profile_id,
+#   ozpcenter_profile.display_name profile_display_name,
+#   auth_user.username profile_username,
+#   auth_group.name profile_group_name,
+#   CASE auth_group.name
+#     WHEN 'APPS_MALL_STEWARD' THEN 1
+#     WHEN 'ORG_STEWARD' THEN 2
+#     WHEN 'USER' THEN 3
+#   END role_priority,
+#   agency.title agency_title,
+#   steward_agency.title steward_agency
+#
+# FROM ozpcenter_profile
+#   JOIN auth_user ON (ozpcenter_profile.user_id = auth_user.id)
+#   JOIN auth_user_groups ON (auth_user_groups.user_id = auth_user.id)
+#   JOIN auth_group ON (auth_user_groups.group_id = auth_group.id)
+#
+#   LEFT JOIN agency_profile ON (ozpcenter_profile.id = agency_profile.profile_id)
+#   LEFT JOIN ozpcenter_agency agency ON (agency_profile.agency_id = agency.id)
+#
+#   LEFT JOIN stewarded_agency_profile ON (ozpcenter_profile.id = stewarded_agency_profile.profile_id)
+#   LEFT JOIN ozpcenter_agency steward_agency ON (stewarded_agency_profile.agency_id = steward_agency.id)
+# ORDER BY role_priority
 
-def get_user_listings(username):
-    """
-    Get User listings
 
-    Returns:
-        Python object of listings
-    """
-    mapping_dict = {}
+def get_sql_statement():
+    schema_class_str = str(connection.SchemaEditorClass)
+    is_deleted = None
+    is_enabled = None
 
-    cursor = connection.cursor()
-    cursor.execute("""
-SELECT
+    if 'sqlite' in schema_class_str:
+        is_deleted = '0'
+        is_enabled = '1'
+    elif 'postgres' in schema_class_str:
+        is_deleted = 'False'
+        is_enabled = 'True'
+    else:
+        raise Exception('Get SQL Statment ENGINE Error')
+
+    sql_statement = '''
+SELECT DISTINCT
   ozpcenter_listing.id,
   ozpcenter_listing.title,
   ozpcenter_listing.approved_date,
@@ -67,7 +96,6 @@ SELECT
   ozpcenter_listing.last_activity_id,
   ozpcenter_listing.listing_type_id,
   ozpcenter_listing.required_listings_id,
-
   ozpcenter_listing.is_deleted,
 
   /* One to Many */
@@ -80,7 +108,7 @@ SELECT
   ozpcenter_agency.short_name agency_short_name,
 
   ozpcenter_listing.small_icon_id,
-  small_image.security_marking small_image_security_marking,
+  small_icon.security_marking small_icon_security_marking,
 
   ozpcenter_listing.large_icon_id,
   large_icon.security_marking large_icon_security_marking,
@@ -92,48 +120,97 @@ SELECT
   large_banner_icon.security_marking large_banner_icon_security_marking,
 
   /* Many to Many */
+  /* Category */
   category_listing.category_id,
-  ozpcenter_category.title,
-  ozpcenter_category.description,
+  ozpcenter_category.title category_title,
+  ozpcenter_category.description category_description,
 
+  /* Contact */
   contact_listing.contact_id contact_id,
   ozpcenter_contact.contact_type_id contact_type_id, /* Check to see if contact_id and contact_type_id is correct*/
-  ozpcenter_contacttype.name,
+  ozpcenter_contacttype.name contact_type_name,
   ozpcenter_contact.secure_phone contact_secure_phone,
   ozpcenter_contact.unsecure_phone contact_unsecure_phone,
   ozpcenter_contact.email contact_email,
   ozpcenter_contact.name contact_name,
   ozpcenter_contact.organization contact_organization,
 
+  /* Tags */
   tag_listing.tag_id,
-  ozpcenter_tag.name,
+  ozpcenter_tag.name tag_name,
 
+  /* Owners */
   owners.profile_id,
-  intent_listing.intent_id
+  owner_profile.display_name owner_display_name,
+  owner_profile.user_id owner_user_id,
+  owner_user.username owner_username,
+
+  /* Intents */
+  intent_listing.intent_id,
+  ozpcenter_intent.action intent_action,
+
+  /* Bookmarks */
+  lib_entries.count bookmark_count
 FROM
   ozpcenter_listing
 /* One to Many Joins */
 JOIN ozpcenter_agency ON (ozpcenter_listing.agency_id = ozpcenter_agency.id)
 JOIN ozpcenter_listingtype ON (ozpcenter_listingtype.id = ozpcenter_listing.listing_type_id)
-JOIN ozpcenter_image small_image ON (small_image.id = ozpcenter_listing.small_icon_id)
+JOIN ozpcenter_image small_icon ON (small_icon.id = ozpcenter_listing.small_icon_id)
 JOIN ozpcenter_image large_icon ON (large_icon.id = ozpcenter_listing.small_icon_id)
 JOIN ozpcenter_image banner_icon ON (banner_icon.id = ozpcenter_listing.small_icon_id)
 JOIN ozpcenter_image large_banner_icon ON (large_banner_icon.id = ozpcenter_listing.small_icon_id)
 /* Many to Many Joins */
+
+/* Categories */
 LEFT JOIN category_listing ON (category_listing.listing_id = ozpcenter_listing.id)
-JOIN ozpcenter_category on (category_listing.category_id = ozpcenter_category.id)
+LEFT JOIN ozpcenter_category on (category_listing.category_id = ozpcenter_category.id)
 
+/* Contacts */
 LEFT JOIN contact_listing ON (contact_listing.listing_id = ozpcenter_listing.id)
-JOIN ozpcenter_contact on (contact_listing.contact_id = ozpcenter_contact.id)
-JOIN ozpcenter_contacttype on (ozpcenter_contact.contact_type_id = ozpcenter_contacttype.id)
+LEFT JOIN ozpcenter_contact on (contact_listing.contact_id = ozpcenter_contact.id)
+LEFT JOIN ozpcenter_contacttype on (ozpcenter_contact.contact_type_id = ozpcenter_contacttype.id)
 
+/* Tags */
 LEFT JOIN tag_listing ON (tag_listing.listing_id = ozpcenter_listing.id)
-JOIN ozpcenter_tag ON (tag_listing.tag_id = ozpcenter_tag.id)
+LEFT JOIN ozpcenter_tag ON (tag_listing.tag_id = ozpcenter_tag.id)
 
+/* Owners */
 LEFT JOIN profile_listing owners ON (owners.listing_id = ozpcenter_listing.id)
+LEFT JOIN ozpcenter_profile owner_profile ON (owners.profile_id = owner_profile.id)
+LEFT JOIN auth_user owner_user ON (owner_profile.user_id = owner_user.id)
 
-LEFT JOIN intent_listing ON ( intent_listing.listing_id = ozpcenter_listing.id);
-          """)
+/* Intent */
+LEFT JOIN intent_listing ON (intent_listing.listing_id = ozpcenter_listing.id)
+LEFT JOIN ozpcenter_intent ON (intent_listing.intent_id = ozpcenter_intent.id)
+
+/* Bookmarks */
+LEFT JOIN (SELECT ozpcenter_applicationlibraryentry.listing_id, count(ozpcenter_applicationlibraryentry.listing_id)
+           FROM ozpcenter_applicationlibraryentry
+           GROUP BY ozpcenter_applicationlibraryentry.listing_id) lib_entries ON (lib_entries.listing_id = ozpcenter_listing.id)
+/*
+Get Listings that are enabled, not deleted, and approved
+*/
+WHERE ozpcenter_listing.is_enabled = {} AND
+      ozpcenter_listing.is_deleted = {} AND
+      ozpcenter_listing.approval_status = 'APPROVED'
+ORDER BY ozpcenter_listing.approved_date DESC;
+    '''.format(is_enabled, is_deleted)
+    return sql_statement
+
+
+def get_user_listings(username):
+    """
+    Get User listings
+
+    Returns:
+        Python object of listings
+    """
+    mapping_dict = {}
+
+    cursor = connection.cursor()
+
+    cursor.execute(get_sql_statement())
     rows = dictfetchall(cursor)
 
     categories_set = set()
@@ -148,13 +225,11 @@ LEFT JOIN intent_listing ON ( intent_listing.listing_id = ozpcenter_listing.id);
                 "is_enabled": row['is_enabled'],
                 "required_listings_id": row['required_listings_id'],
                 "is_private": row['is_private'],
-                "banner_icon_id": row['banner_icon_id'],
-                "large_banner_icon_id": row['large_banner_icon_id'],
                 "total_rate2": row['total_rate2'],
                 "id": row['id'],
                 "avg_rate": row['avg_rate'],
                 "unique_name": row['unique_name'],
-                "short_name": row['short_name'],
+
                 "approved_date": row['approved_date'],
                 "total_rate5": row['total_rate5'],
                 "requirements": row['requirements'],
@@ -173,36 +248,59 @@ LEFT JOIN intent_listing ON ( intent_listing.listing_id = ozpcenter_listing.id);
                 "title": row['title'],
                 "description_short": row['description_short'],
                 "total_reviews": row['total_reviews'],
-                "small_icon_id": row['small_icon_id'],
                 "listing_type_id": row['listing_type_id'],
                 "launch_url": row['launch_url'],
-                "large_icon_id": row['large_icon_id'],
                 "edited_date": row['edited_date'],
                 "description": row['description'],
                 "total_votes": row['total_votes'],
+                # One to One
+                "agency": {'title': row['agency_title'],
+                           'short_name': row['agency_short_name']},
+                "small_icon": {"id": row['small_icon_id'],
+                               'url': None,
+                               "security_marking": row['small_icon_security_marking']},
+                "large_icon": {"id": row['large_icon_id'],
+                               'url': None,
+                               "security_marking": row['large_icon_security_marking']},
+                "banner_icon": {"id": row['banner_icon_id'],
+                                'url': None,
+                                "security_marking": row['banner_icon_security_marking']},
+                "large_banner_icon": {"id": row['large_banner_icon_id'],
+                                      'url': None,
+                                      "security_marking": row['large_banner_icon_security_marking']}
             }
 
             # Many to Many
             if row['category_id']:
-                mapping_dict[row['id']]['categories'] = {row['category_id']: None}
+                mapping_dict[row['id']]['categories'] = {row['category_id']:
+                                                         {'title': row['category_title'],
+                                                          'description': row['category_description']}}
                 categories_set.add(row['category_id'])
             else:
                 mapping_dict[row['id']]['categories'] = {}
 
             if row['tag_id']:
-                mapping_dict[row['id']]['tags'] = {row['tag_id']: None}
+                mapping_dict[row['id']]['tags'] = {row['tag_id']: {'name': row['tag_name']}}
                 tags_set.add(row['tag_id'])
             else:
                 mapping_dict[row['id']]['tags'] = {}
 
             if row['contact_id']:
-                mapping_dict[row['id']]['contacts'] = {row['contact_id']: None}
+                mapping_dict[row['id']]['contacts'] = {row['contact_id']: {'id': row['contact_id'],
+                                                     'secure_phone': row['contact_secure_phone'],
+                                                     'unsecure_phone': row['contact_unsecure_phone'],
+                                                     'email': row['contact_email'],
+                                                     'name': row['contact_name'],
+                                                     'organization': row['contact_organization'],
+                                                     'contact_type': {'name': row['contact_type_name']}
+                    }}
                 contacts_set.add(row['contact_id'])
             else:
                 mapping_dict[row['id']]['contacts'] = {}
 
             if row['profile_id']:
-                mapping_dict[row['id']]['owners'] = {row['profile_id']: None}
+                mapping_dict[row['id']]['owners'] = {row['profile_id']: {'display_name': row['owner_display_name'],
+                    'user': {'username': row['owner_username']}}}
                 profile_set.add(row['profile_id'])
             else:
                 mapping_dict[row['id']]['owners'] = {}
@@ -216,22 +314,31 @@ LEFT JOIN intent_listing ON ( intent_listing.listing_id = ozpcenter_listing.id);
         else:
             if row['category_id']:
                 if row['category_id'] not in mapping_dict[row['id']]['categories']:
-                    mapping_dict[row['id']]['categories'][row['category_id']] = None
+                    mapping_dict[row['id']]['categories'][row['category_id']] = {'title': row['category_title'],
+                                                                                 'description': row['category_description']}
                     categories_set.add(row['category_id'])
 
             if row['tag_id']:
                 if row['tag_id'] not in mapping_dict[row['id']]['tags']:
-                    mapping_dict[row['id']]['tags'][row['tag_id']] = None
+                    mapping_dict[row['id']]['tags'][row['tag_id']] = {'name': row['tag_name']}
                     tags_set.add(row['tag_id'])
 
             if row['contact_id']:
                 if row['contact_id'] not in mapping_dict[row['id']]['contacts']:
-                    mapping_dict[row['id']]['contacts'][row['contact_id']] = None
+                    mapping_dict[row['id']]['contacts'][row['contact_id']] = {'id': row['contact_id'],
+                                                         'secure_phone': row['contact_secure_phone'],
+                                                         'unsecure_phone': row['contact_unsecure_phone'],
+                                                         'email': row['contact_email'],
+                                                         'name': row['contact_name'],
+                                                         'organization': row['contact_organization'],
+                                                         'contact_type': {'name': row['contact_type_name']}
+                        }
                     contacts_set.add(row['contact_id'])
 
             if row['profile_id']:
                 if row['profile_id'] not in mapping_dict[row['id']]['owners']:
-                    mapping_dict[row['id']]['owners'][row['profile_id']] = None
+                    mapping_dict[row['id']]['owners'][row['profile_id']] = {'display_name': row['owner_display_name'],
+                        'user': {'username': row['owner_username']}}
                     profile_set.add(row['profile_id'])
 
             if row['intent_id']:
@@ -239,56 +346,15 @@ LEFT JOIN intent_listing ON ( intent_listing.listing_id = ozpcenter_listing.id);
                     mapping_dict[row['id']]['intents'][row['intent_id']] = None
                     intents_set.add(row['intent_id'])
 
-    profiles = {}
-    for profile in models.Profile.objects.filter(id__in=profile_set).all():
-        profiles[profile.id] = {'id': profile.id, 'display_name': profile.display_name}
-
-    tags = {}
-    for tag_instance in models.Tag.objects.filter(id__in=tags_set).all():
-        tags[tag_instance.id] = {'name': tag_instance.name}
-
-    categories = {}
-    for category_instance in models.Category.objects.filter(id__in=categories_set).all():
-        categories[category_instance.id] = {'title': category_instance.title,
-                                      'description': category_instance.description}
-
-    intents = {}
-    for intent_instance in models.Intent.objects.filter(id__in=intents_set).all():
-        intents[intent_instance.id] = {'action': intent_instance.action}
-
-    contacts = {}
-    for contact_instance in models.Contact.objects.filter(id__in=contacts_set).all():
-        contacts[contact_instance.id] = {'id': contact_instance.id,
-                                         'secure_phone': contact_instance.secure_phone,
-                                         'unsecure_phone': contact_instance.unsecure_phone,
-                                         'email': contact_instance.email,
-                                         'name': contact_instance.name,
-                                         'organization': contact_instance.organization,
-                                         # 'contact_type': contact_instance.contact_type.id  # JOIN
-                                         }
-
-    print(categories_set)
-    print(categories)
     for profile_key in mapping_dict:
-
         profile_map = mapping_dict[profile_key]
-        print(profile_map['categories'])
-        profile_map['owners'] = [profiles[p_key] for p_key in profile_map['owners']]
-        profile_map['tags'] = [tags[p_key] for p_key in profile_map['tags']]
-        profile_map['categories'] = [categories[p_key] for p_key in profile_map['categories']]
-        profile_map['contacts'] = [contacts[p_key] for p_key in profile_map['contacts']]
-        profile_map['intents'] = [intents[p_key] for p_key in profile_map['intents']]
+        profile_map['owners'] = [profile_map['owners'][p_key] for p_key in profile_map['owners']]
+        profile_map['tags'] = [profile_map['tags'][p_key] for p_key in profile_map['tags']]
+        profile_map['categories'] = [profile_map['categories'][p_key] for p_key in profile_map['categories']]
+        profile_map['contacts'] = [profile_map['contacts'][p_key] for p_key in profile_map['contacts']]
+        profile_map['intents'] = [profile_map['intents'][p_key] for p_key in profile_map['intents']]
 
     return list(mapping_dict.values())
-
-    featured_listings_raw = models.Listing.objects.for_user_organization_minus_security_markings(
-        username).filter(
-            is_featured=True,
-            approval_status=models.Listing.APPROVED,
-            is_enabled=True,
-            is_deleted=False).all()
-    featured_listings_raw = listing_serializers.ListingSerializer.setup_eager_loading(featured_listings_raw)
-    return [model_to_dict(listing) for listing in featured_listings_raw]
 
 
 def get_storefront(username, pre_fetch=False):
