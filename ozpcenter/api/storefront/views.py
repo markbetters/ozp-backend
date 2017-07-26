be8563ebe6d16f9b122398428bf2a50cbbab7d95
Manny Rivera <mannyrivera2010@gmail.com>
2017-07-26 15:03:45 -0400

    feat(storefront): Created ViewSet for Storefront

--- ozpcenter/api/storefront/views.py
+++ ozpcenter/api/storefront/views.py
@@ -24,6 +24,7 @@ import logging
 # from django.core.cache import cache
 from rest_framework.decorators import api_view
 from rest_framework.decorators import permission_classes
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from ozpcenter import permissions
@@ -46,67 +47,78 @@ def MetadataView(request):
     return Response(data)
 
 
-@api_view(['GET'])
-@permission_classes((permissions.IsUser, ))
-def StorefrontView(request):
+class StorefrontViewSet(viewsets.ViewSet):
     """
-    Featured, recent, and most popular listings
-    ---
-    serializer: ozpcenter.api.storefront.serializers.StorefrontSerializer
+    A simple ViewSet for listing or retrieving users.
     """
-    # return Response(model_access.get_storefront_new(request.user, request))
-    data, extra_data = model_access.get_storefront(request.user, True)
-    serializer = serializers.StorefrontSerializer(data,
-        context={'request': request})
+    permission_classes = (permissions.IsUser,)
 
-    serialized_data = serializer.data
+    def list(self, request):
+        """
+        Featured, recent, and most popular listings
+        ---
+        serializer: ozpcenter.api.storefront.serializers.StorefrontSerializer
+        """
+        # return Response(model_access.get_storefront_new(request.user, request))
+        data, extra_data = model_access.get_storefront(request.user, True)
+        serializer = serializers.StorefrontSerializer(data,
+            context={'request': request})
 
-    # extra_data = {'recommended_entry_data': {'Baseline': {'ms_took': 1204.51953125, 'weight': 1.0, 'recommendations': [
-    #     [11, 8.5], [112, 8.0], [85, 7.0], [86, 7.0], [87, 7.0], [88, 7.0], [89, 7.0], [90, 7.0], [62, 6.0],
-    #     [81, 6.0], [21, 5.5], [1, 5.0], [111, 5.0], [113, 5.0], [114, 5.0], [64, 4.0], [66, 4.0], [68, 4.0],
-    #     [70, 4.0], [72, 4.0]]}}}
-    # TO
-    # {listing_id#1 : {friendly_name#1: {'raw_score': score#1, 'weight': weight#1}
-    #                  friendly_name#2: {'raw_score': score#1, 'weight': weight#1}},
-    # listing_id#2 : {friendly_name#1: {'raw_score': score#1, 'weight': weight#1},
-    #                  friendly_name#2: {'raw_score': score#1, 'weight': weight#1}}
-    # }
-    # Insert into each serialized listing into serialized_data
-    if 'recommended_entry_data' in extra_data:
-        recommended_entry_data = extra_data['recommended_entry_data']
+        serialized_data = serializer.data
 
-        listing_recommend_data = {}
+        # extra_data = {'recommended_entry_data': {'Baseline': {'ms_took': 1204.51953125, 'weight': 1.0, 'recommendations': [
+        #     [11, 8.5], [112, 8.0], [85, 7.0], [86, 7.0], [87, 7.0], [88, 7.0], [89, 7.0], [90, 7.0], [62, 6.0],
+        #     [81, 6.0], [21, 5.5], [1, 5.0], [111, 5.0], [113, 5.0], [114, 5.0], [64, 4.0], [66, 4.0], [68, 4.0],
+        #     [70, 4.0], [72, 4.0]]}}}
+        # TO
+        # {listing_id#1 : {friendly_name#1: {'raw_score': score#1, 'weight': weight#1}
+        #                  friendly_name#2: {'raw_score': score#1, 'weight': weight#1}},
+        # listing_id#2 : {friendly_name#1: {'raw_score': score#1, 'weight': weight#1},
+        #                  friendly_name#2: {'raw_score': score#1, 'weight': weight#1}}
+        # }
+        # Insert into each serialized listing into serialized_data
+        if 'recommended_entry_data' in extra_data:
+            recommended_entry_data = extra_data['recommended_entry_data']
 
-        for friendly_name in recommended_entry_data:
-            current_weight = recommended_entry_data[friendly_name]['weight']
-            recommendations = recommended_entry_data[friendly_name]['recommendations']
+            listing_recommend_data = {}
 
-            for current_recommendations in recommendations:
-                current_listing = current_recommendations[0]
-                current_score = current_recommendations[1]
+            for friendly_name in recommended_entry_data:
+                current_weight = recommended_entry_data[friendly_name]['weight']
+                recommendations = recommended_entry_data[friendly_name]['recommendations']
 
-                if current_listing in listing_recommend_data:
-                    listing_recommend_data[current_listing][friendly_name] = {'raw_score': current_score, 'weight': current_weight}
-                else:
-                    listing_recommend_data[current_listing] = {}
-                    listing_recommend_data[current_listing][friendly_name] = {'raw_score': current_score, 'weight': current_weight}
+                for current_recommendations in recommendations:
+                    current_listing = current_recommendations[0]
+                    current_score = current_recommendations[1]
 
-        recommendation_serialized_list = []
+                    if current_listing in listing_recommend_data:
+                        listing_recommend_data[current_listing][friendly_name] = {'raw_score': current_score, 'weight': current_weight}
+                    else:
+                        listing_recommend_data[current_listing] = {}
+                        listing_recommend_data[current_listing][friendly_name] = {'raw_score': current_score, 'weight': current_weight}
 
-        for serialized_listing in serialized_data['recommended']:
-            serialized_listing_id = serialized_listing['id']
+            recommendation_serialized_list = []
 
-            serialized_listing['_score'] = listing_recommend_data[serialized_listing_id]
+            for serialized_listing in serialized_data['recommended']:
+                serialized_listing_id = serialized_listing['id']
 
-            recommendation_serialized_list.append(serialized_listing)
+                serialized_listing['_score'] = listing_recommend_data[serialized_listing_id]
 
-        serialized_data['recommended'] = recommendation_serialized_list
+                recommendation_serialized_list.append(serialized_listing)
 
-    # request_username = request.user.username
-    # cache_key = 'storefront-{0}'.format(request_username)
-    # cache_data = cache.get(cache_key)
-    # if not cache_data:
-    #    cache_data = serializer.data
-    #    cache.set(cache_key, cache_data, timeout=settings.GLOBAL_SECONDS_TO_CACHE_DATA)
-    # return Response(cache_data)
-    return Response(serialized_data)
+            serialized_data['recommended'] = recommendation_serialized_list
+
+        # request_username = request.user.username
+        # cache_key = 'storefront-{0}'.format(request_username)
+        # cache_data = cache.get(cache_key)
+        # if not cache_data:
+        #    cache_data = serializer.data
+        #    cache.set(cache_key, cache_data, timeout=settings.GLOBAL_SECONDS_TO_CACHE_DATA)
+        # return Response(cache_data)
+        return Response(serialized_data)
+
+    def retrieve(self, request, pk=None):
+        if pk == 'recent':
+            return Response(['recent'])
+        elif pk == 'featured':
+            return Response(['featured'])
+        return Response([])
